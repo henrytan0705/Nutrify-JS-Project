61fb3c5a452de2906ffde533f5954cf576745042
HenryTan0705 <htan0705@gmail.com>
2019-08-13 17:27:29 -0400

    Connect input data to api fetch and then connect data fetched to chart, clear chart of previous data upon each api fetch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 // import '../assets/stylesheets/css_reset.css';
 // import '../assets/stylesheets/application.css';
+var chart = null;
 
-document.addEventListener("click", (e) => {
+document.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    let formValue = document.getElementById("formValue").value;
+    
+    let parsedData = {
+        "title": "",
+        "ingr":[formValue]
+    };
 
     function postData(url = '', data = {}) {
         return fetch(url, {
@@ -19,36 +27,111 @@ document.addEventListener("click", (e) => {
         })
             .then(response => response.json());
     }
-    let foodData;
-    
-    postData('https://api.edamam.com/api/nutrition-details?app_id=2933ad3c&app_key=2b27a8fdec1d51e05cca6a634d8e9204', {
-        "title": "Fresh Ham Roasted With Rye Bread and Dried Fruit Stuffing",
-        // "prep": "1. Have your butcher bone and butterfly the ham and score the fat in a diamond pattern. ...",
-        // "yield": "About 15 servings",
-        // "yield": "About 1 serving",
-        "ingr": [
-            "1 fresh ham, about 1 pound",
-            // "7 cloves garlic, minced",
-            // "1 tablespoon caraway seeds, crushed",
-            "4 teaspoons salt",
-            // "Freshly ground pepper to taste",
-            // "1 teaspoon olive oil",
-            // "1 medium onion, peeled and chopped",
-            // "3 cups sourdough rye bread, cut into 1/2-inch cubes",
-            // "1 1/4 cups coarsely chopped pitted prunes",
-            // "1 1/4 cups coarsely chopped dried apricots",
-            // "1 large tart apple, peeled, cored and cut into 1/2-inch cubes",
-            // "2 teaspoons chopped fresh rosemary",
-            // "1 egg, lightly beaten",
-            // "1 cup chicken broth, homemade or low-sodium canned"
-        ]
-    })
+
+    postData('https://api.edamam.com/api/nutrition-details?app_id=2933ad3c&app_key=2b27a8fdec1d51e05cca6a634d8e9204', parsedData)
         .then(data => {
-            debugger
-            return console.log(JSON.stringify(data))
+            // debugger
+            let nutritionTypes = Object.values(data.totalNutrients).map(i => i.label)
+            let nutritionQuantity = Object.values(data.totalNutrients).map(i => i.quantity);
+            if (chart !== null) chart.destroy();
+
+            let ctx = document.getElementById("polarChart");
+            chart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: nutritionTypes,
+                    datasets: [{
+                        label: 'Nutritional Breakdown',
+                        data: nutritionQuantity,
+                        backgroundColor: [
+                            'rgba(255,0,0,.7)',
+                            'rgba(100,50,0,.7)',
+                            'rgba(0,100,100,.7)',
+                            'rgba(150,0,0,.7)',
+                            'rgba(0,50,0,.7)',
+                            'rgba(0,0,200,.7)',
+                            'rgba(255,0,0,.7)',
+                            'rgba(100,50,0,.7)',
+                            'rgba(0,100,100,.7)',
+                            'rgba(150,0,0,.7)',
+                            'rgba(0,50,0,.7)',
+                            'rgba(0,0,200,.7)',
+                            'rgba(255,0,0,.7)',
+                            'rgba(100,50,0,.7)',
+                            'rgba(0,100,100,.7)',
+                            'rgba(150,0,0,.7)',
+                            'rgba(0,50,0,.7)',
+                            'rgba(0,0,200,.7)',
+                            'rgba(255,0,0,.7)',
+                            'rgba(100,50,0,.7)',
+                            'rgba(0,100,100,.7)',
+                            'rgba(150,0,0,.7)',
+                            'rgba(0,50,0,.7)',
+                            'rgba(0,0,200,.7)',
+                        ],
+                        borderColor: [
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000',
+                            '#000'
+                     
+                        ],
+                        borderWidth: 0,
+                        hoverBorderColor: [
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                            '#000000',
+                        ],
+                        hoverBorderWidth: 2
+                    }]
+                },
+
+            });
         })
         .catch(error => {
             debugger
            return console.error(error())
         })
+
+
+
+    // debugger
+
+    
+    
 })
